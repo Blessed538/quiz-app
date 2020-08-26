b5a98033625dee8f5fc52d0317ea41b5073e3cdb
unknown <blesseddominic98@gmail.com>
2020-08-26 13:21:17 +0100

    first commit

---
+++ src/components/Quizdata.js
@@ -0,0 +1,34 @@
+export const quizdata = [
+
+  {
+  id:1,
+  question: "what is the meaning of HOC",
+  option:["high-order-component", "higher-order-component", "higher-odd-program"],
+  answer:"higher-order-component"
+  },
+
+  {
+   id:2,
+   question: "where should a state be placed in react?",
+   option:["app.js", "index.js", "html.js"],
+   answer:"app.js"
+ },
+ {
+   id:3,
+   question: "where should a state be placed in react?",
+   option:["app.js", "index.js", "html.js"],
+   answer:"app.js"
+ },
+ {
+   id:4,
+   question: "where should a state be placed in react?",
+   option:["app.js", "index.js", "html.js"],
+   answer:"app.js"
+ },
+ {
+   id:5,
+   question: "where should a state be placed in react?",
+   option:["app.js", "index.js", "html.js"],
+   answer:"app.js"
+ },
+]
